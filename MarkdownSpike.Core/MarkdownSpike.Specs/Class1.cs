c764130a927078ea26b286c2791ffa513b2a9ae7
David Alpert <david@spinthemoose.com>
2013-03-04 22:18:35 -0600

    step

---
+++ MarkdownSpike.Core/MarkdownSpike.Specs/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MarkdownSpike.Specs
+{
+    using Machine.Specifications;
+    using NUnit.Framework;
+    using developwithpassion.specifications.nsubstitue;
+
+    public class Class1 : Observes<PageRepository>
+    {
+        private Because of = () =>
+        {
+            
+        };
+
+        private It should_do_something = () =>
+        {
+
+        };
+    }
+
+    [TestFixture]
+    public class test
+    {
+        [Test]
+        public void Test()
+        {
+            PageRepository repo = new PageRepository();
+        }
+    }
+
+    public class PageRepository
+    {
+    }
+}
